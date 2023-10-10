941a666e2b428a07bb81c6c5dc8cdc1996edfb39
ou5ama <lasri.oussama@gmail.com>
2023-10-10 15:06:22 +0100

    creating a new branch 'auth' and rebasing 'sign' to it

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes } from '@angular/router';
 import { SignComponent } from './sign/sign.component';
 
 
+
+const authRoutes: Routes = [
+  {path: 'sign', component: SignComponent},
+]
 
 @NgModule({
   declarations: [
