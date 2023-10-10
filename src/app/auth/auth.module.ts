99518c446b23574be81f512508848f69b8111e1f
ou5ama <lasri.oussama@gmail.com>
2023-10-10 19:03:51 +0100

    create a login components, and add the path in the auth module

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { SignComponent } from './sign/sign.component';
+import { LoginComponent } from './login/login.component';
 
-const authRoutes: Routes = [{ path: 'sign', component: SignComponent }];
+const authRoutes: Routes = [
+  { path: 'sign', component: SignComponent },{path: 'login', component: LoginComponent}];
 
 @NgModule({
-  declarations: [SignComponent],
+  declarations: [SignComponent, LoginComponent],
   imports: [
     CommonModule,
     RouterModule.forChild(authRoutes), // Use 'forChild' to configure child routes
