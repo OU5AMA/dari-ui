e922bf6e01370a4950ac2ac43f09480dc54b4e28
ou5ama <lasri.oussama@gmail.com>
2023-10-11 19:16:38 +0100

    create a forget-password components and add the its path in the route array

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,14 +3,16 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { SignComponent } from './sign/sign.component';
 import { LoginComponent } from './login/login.component';
+import { ForgetPasswordComponent } from './forget-password/forget-password.component';
 
 const authRoutes: Routes = [
   { path: 'sign', component: SignComponent },
   { path: 'login', component: LoginComponent },
+  {path: 'forget-password', component: ForgetPasswordComponent}
 ];
 
 @NgModule({
-  declarations: [SignComponent, LoginComponent],
+  declarations: [SignComponent, LoginComponent, ForgetPasswordComponent],
   imports: [
     CommonModule,
     RouterModule.forChild(authRoutes), // Use 'forChild' to configure child routes
