e73d48a524ca80321097a5e229b0433aa7ba9473
ou5ama <lasri.oussama@gmail.com>
2023-10-12 10:34:58 +0100

    add an interface and @Input decorator to the Login component

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
+
+interface LoginData{
+  username: string;
+  password: string;
+}
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
-
+  @Input() data: LoginData;
 }
