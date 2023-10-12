a8105c9ebe639b760dd38c5293db01e5fe4e6a8d
ou5ama <lasri.oussama@gmail.com>
2023-10-12 14:15:29 +0100

    create Reactive form for forgetPassword.ts

--- src/app/auth/forget-password/forget-password.component.ts
+++ src/app/auth/forget-password/forget-password.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 
 @Component({
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./forget-password.component.scss']
 })
 export class ForgetPasswordComponent {
+  forgetPasswordForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder){
+    this.forgetPasswordForm = this.formBuilder.group({
+      email: ['', Validators.required]
+    })
+  }
+
+  onForgetPassword(){
+    console.log("you forgot your password")
+  }
 }
